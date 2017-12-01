4f8b13e0f3eb3db9eb11d8ea9d578ce61056537a
Mattias Zurkovic <mattias@zurkovic.com>
2017-12-01 13:28:39 -0800

    add marker creation function and test

--- public/javascripts/googleMaps.js
+++ public/javascripts/googleMaps.js
@@ -1,8 +1,11 @@
 var map, infoWindow;
+
 function initMap() {
+  latLng = new google.maps.LatLng(49, 123.644)
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    zoom: 10
   });
   infoWindow = new google.maps.InfoWindow;
 
@@ -22,6 +25,9 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  addMarker(latLng);
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -30,4 +36,13 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
+}
+
+function addMarker(coords) {
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    visible: true
+  });
 }
